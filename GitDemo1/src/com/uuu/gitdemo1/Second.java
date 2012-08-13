bca2b4229bd5f87fbfbc79abf664e0433f655fdc
Mark <mark@patristar.com>
2012-08-13 19:59:32 +0800

    Add an activity and layout xml
    Signed-off-by: Mark <mark@patristar.com>

---
+++ GitDemo1/src/com/uuu/gitdemo1/Second.java
@@ -0,0 +1,15 @@
+package com.uuu.gitdemo1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Second extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second);
+	}
+
+}
